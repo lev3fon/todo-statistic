958e3ca693e7f36315f9d44f880246a557eebc96
Дарья Бабарыкина <daria@MacBook-Pro-Dara.local>
2021-03-15 15:23:33 +0500

    Task done, full task in Trifonov rep

--- index.js
+++ index.js
@@ -16,9 +16,42 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            let file = getFiles()
+            let result = []
+            for (let f of file) {
+                f = f.split('\n')
+                for (let i of f) {
+                    if (i.indexOf('// TODO') > -1) {
+                        result.push(i)
+                    }
+                }
+            }
+            //return result
+            console.log(result)
+            break;
+
+        /*case 'important':
+            let imp = show()
+            let re = []
+            for (i of imp){
+                if (i.indexOf('!') > -1){
+                     re.push(i)
+                }
+
+            }*/
+            console.log(re)
+
+
+            break;
+
+
+
+
         default:
             console.log('wrong command');
             break;
+            
     }
 }
 
